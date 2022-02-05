e77304daf5d96f3e484b892e3e82012570359515
AR3G0 <noah109carrico@gmail.com>
2022-02-05 16:27:06 -0600

    Added room numbers
    Within the room controller script, I have a loop generate a room # for each section of the map.

--- Eight Hours Till Dawn/Assets/scripts/roomController.cs
+++ Eight Hours Till Dawn/Assets/scripts/roomController.cs
@@ -1,10 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class roomController : MonoBehaviour
 {
     public GameObject roomText;
+    public Text roomName;
+    public Transform parent;
 
     // Start is called before the first frame update
     void Start()
@@ -20,64 +23,86 @@ public class roomController : MonoBehaviour
         }
 
         #region
+        
         // Store the center of each room 
-        roomDataArray[0, 0] = -10; // X position
-        roomDataArray[0, 1] = -5; // Y position
+        // Room 16
+        roomDataArray[0, 1] = -10; // X position
+        roomDataArray[0, 2] = -5; // Y position
 
-        roomDataArray[1, 0] = -10; // X position
-        roomDataArray[1, 1] = -1; // Y position
+         
+        // room 1
+        roomDataArray[1, 1] = -10; // X position
+        roomDataArray[1, 2] = -1; // Y position
 
-        roomDataArray[2, 0] = -6; // X position
-        roomDataArray[2, 1] = -3; // Y position
+        // room 2
+        roomDataArray[2, 1] = -0; // X position
+        roomDataArray[2, 2] = -1; // Y position
+        
+        roomDataArray[3, 1] = 0; // X position
+        roomDataArray[3, 2] = -6; // Y position
 
-        roomDataArray[3, 0] = 0; // X position
-        roomDataArray[3, 1] = -5; // Y position
+        // Room 4
+        roomDataArray[4, 1] = 0; // X position
+        roomDataArray[4, 2] = -4; // Y position
 
-        roomDataArray[4, 0] = 0; // X position
-        roomDataArray[4, 1] = -3; // Y position
+        roomDataArray[5, 1] = 7; // X position
+        roomDataArray[5, 2] = -3; // Y position
 
-        roomDataArray[5, 0] = 7; // X position
-        roomDataArray[5, 1] = -3; // Y position
+        roomDataArray[6, 1] = 11; // X position
+        roomDataArray[6, 2] = -3; // Y position
 
-        roomDataArray[6, 0] = 11; // X position
-        roomDataArray[6, 1] = -3; // Y position
+        roomDataArray[7, 1] = -10; // X position
+        roomDataArray[7, 2] = 1; // Y position
 
-        roomDataArray[7, 0] = -10; // X position
-        roomDataArray[7, 1] = 1; // Y position
+        roomDataArray[8, 1] = 3; // X position
+        roomDataArray[8, 2] = 1; // Y position
 
-        roomDataArray[8, 0] = 3; // X position
-        roomDataArray[8, 1] = 1; // Y position
+        roomDataArray[9, 1] = 9; // X position
+        roomDataArray[9, 2] = 2; // Y position
 
-        roomDataArray[9, 0] = 9; // X position
-        roomDataArray[9, 1] = 2; // Y position
+        roomDataArray[10, 1] = -10; // X position
+        roomDataArray[10, 2] = 3; // Y position
 
-        roomDataArray[10, 0] = -10; // X position
-        roomDataArray[10, 1] = 3; // Y position
+        roomDataArray[11, 1] = -4; // X position
+        roomDataArray[11, 2] = 3; // Y position
 
-        roomDataArray[11, 0] = -4; // X position
-        roomDataArray[11, 1] = 3; // Y position
+        roomDataArray[12, 1] = 3; // X position
+        roomDataArray[12, 2] = 4; // Y position
 
-        roomDataArray[12, 0] = 3; // X position
-        roomDataArray[12, 1] = 4; // Y position
+        roomDataArray[13, 1] = -10; // X position
+        roomDataArray[13, 2] = 6; // Y position
 
-        roomDataArray[13, 0] = -10; // X position
-        roomDataArray[13, 1] = 6; // Y position
-
-        roomDataArray[14, 0] = 8; // X position
-        roomDataArray[14, 1] = 6; // Y position
-
-        //roomDataArray[15, 0] = ; // X position
-        //roomDataArray[15, 1] = ; // Y position
+        // room 15
+        roomDataArray[14, 1] = 8; // X position
+        roomDataArray[14, 2] = 6; // Y position
+        
+        roomDataArray[15, 1] = -6; // X position
+        roomDataArray[15, 2] = -5; // Y position
+      
         #endregion // room positions
 
         // loop through every room and create a text prefab at its center
         for (var i = 0; i < roomDataArray.GetLength(0); i++)
         {
+            // get the x and y positions of the rooms center 
             var xPos = roomDataArray[i, 1];
             var yPos = roomDataArray[i, 2];
 
+            // create a new text prefab and store it in a var
             var newText = Instantiate(roomText);
-            newText.transform.position = new Vector2 (xPos, yPos);
+
+            // retreive the transform of the canvas object
+            newText.transform.SetParent(parent);
+
+
+            // Convert the xPos and yPos into coordinates on the screen, then store it.
+            Vector3 PosInScreen = Camera.main.WorldToScreenPoint(new Vector2(xPos, yPos));
+
+            // move each text prefab to their new location in the world
+            newText.transform.position = PosInScreen;
+
+            // in the objects text componet set the value to the room number
+            roomName.text = "Room " + (i + 1);
         }
     }
 
